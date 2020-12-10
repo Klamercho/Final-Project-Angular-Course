c9139b2cfc083511f6956511ec53c7337fa7ba4f
Klamercho <43107427+Klamercho@users.noreply.github.com>
2020-12-10 20:27:51 +0200

    Add files via upload

---
+++ routers/index.js
@@ -0,0 +1,13 @@
+const routers = [
+    {userRouter: require('./userRouter')},
+    {careersRouter: require('./careersRouter')},
+    {clientsRouter: require('./clientsRouter')}
+
+]
+
+module.exports = (router) => {
+    return routers.reduce((acc, curr) => {
+        const key = Object.keys(curr)[0];
+        return Object.assign(acc, { [key]: curr[key](router) });
+    }, {});
+};
