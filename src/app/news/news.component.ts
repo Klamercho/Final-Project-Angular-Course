c25a6e0eed64c2432b6b5b130382c26d5bea7d11
Klamercho <43107427+Klamercho@users.noreply.github.com>
2020-12-18 00:21:14 +0200

    Updated files

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-news',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
+  errorMessage = '';
+  allNews;
   ngOnInit(): void {
+    this.getAllNews().subscribe({
+      next: (data) => {
+        console.log(data);
+        this.allNews = data;
+      },
+      error: (err) => {
+        this.errorMessage = 'ERROR!'
+      }
+    });
+
   }
 
+  getAllNews(): Observable<any> {
+    return this.http.get('http://localhost:1234/news/allnews')
+  }
 }
